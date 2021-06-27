a9f517d4b47acd1270c820c8e6840c650bdff32a
Ariadna Camino <ariadna.caminoc.cl>
2021-06-27 14:32:36 -0400

    fix: eslint

---
+++ src/routes/admin.js
@@ -0,0 +1,11 @@
+const KoaRouter = require('koa-router');
+
+const router = new KoaRouter();
+
+// router.post('administrator.create', '/', async (ctx) => {
+// console.log('pribadno');
+// const admin = await ctx.orm.administrator.findAll();
+// console.log(admin.length);
+// });
+
+module.exports = router;
