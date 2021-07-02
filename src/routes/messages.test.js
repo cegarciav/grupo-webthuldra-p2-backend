fac0d812c29b81a47ae04784cee89aeacce7f55a
cegarciav <cegarcia2@uc.cl>
2021-07-02 01:49:20 -0400

    feat: add tests for message endpoints

--- src/routes/messages.test.js
+++ src/routes/messages.test.js
@@ -4,7 +4,7 @@ const app = require('../app');
 
 const request = supertest(app.callback());
 
-describe('Deals routes', () => {
+describe('Messages routes', () => {
   let authCustomer;
   const customerFields = {
     id: uuid(),
@@ -27,13 +27,6 @@ describe('Deals routes', () => {
     description: 'Una tienda que vende productos',
     ownerId: ownerFields.id,
   };
-  const store2Fields = {
-    id: uuid(),
-    address: 'Fake Street 124',
-    name: 'Tienda 2',
-    description: 'Otra tienda que vende productos',
-    ownerId: ownerFields.id,
-  };
   const productsFields = [
     {
       id: uuid(),
@@ -51,22 +44,25 @@ describe('Deals routes', () => {
       storeId: storeFields.id,
     },
   ];
-  const product2ndStore = {
-    id: uuid(),
-    name: 'Producto 3',
-    stock: 100,
-    price: 10000,
-    unit: 'kg',
-    storeId: store2Fields.id,
-  };
   const dealFields = {
-    products: [
-      {
-        id: productsFields[1].id,
-        amount: 10,
-      },
-    ],
+    id: uuid(),
+    status: 'abierto',
+    customerId: customerFields.id,
+    storeId: storeFields.id,
   };
+  const purchaseFields = {
+    dealId: dealFields.id,
+    productId: productsFields[0].id,
+    amount: 10,
+  };
+  const messageFields = {
+    id: uuid(),
+    dealId: dealFields.id,
+    sender: 'store',
+    senderId: storeFields.id,
+    text: 'new message just for the GET test',
+  };
+
   beforeAll(async () => {
     await app.context.orm.sequelize.sync({ force: true });
     await app.context.orm.user.create(customerFields);
@@ -77,26 +73,22 @@ describe('Deals routes', () => {
       .send({ email: customerFields.email, password: customerFields.password });
     authCustomer = authCustomerResponse.body;
     await app.context.orm.store.create(storeFields);
-    await app.context.orm.store.create(store2Fields);
-    await app.context.orm.product.create(product2ndStore);
     await app.context.orm.product.bulkCreate(productsFields);
+    await app.context.orm.deal.create(dealFields);
+    await app.context.orm.purchase.create(purchaseFields);
+    await app.context.orm.message.create(messageFields);
   });
   afterAll(async () => {
     await app.context.orm.sequelize.close();
   });
-  describe('POST api/deals/:deal_id/mesagges', () => {
+  describe('POST api/deals/:deal_id/messages', () => {
     let createResponse;
+    const messagesFields = {
+      text: 'message',
+    };
     beforeAll(async () => {
-      const createdDeal = await request
-        .post(`/api/stores/${storeFields.id}/deals`)
-        .set('Content-type', 'application/json')
-        .send(dealFields)
-        .auth(authCustomer.accessToken, { type: 'bearer' });
-      const messagesFields = {
-        text: 'message',
-      };
       createResponse = await request
-        .post(`/api/deals/${createdDeal.id}/messages`)
+        .post(`/api/deals/${dealFields.id}/messages`)
         .set('Content-type', 'application/json')
         .send(messagesFields)
         .auth(authCustomer.accessToken, { type: 'bearer' });
@@ -106,6 +98,38 @@ describe('Deals routes', () => {
     });
     test('response with a json body type', async () => {
       expect(createResponse.type).toEqual('application/json');
+    });
+    test('response body contains sender type customer', async () => {
+      expect(createResponse.body.sender).toEqual('customer');
+    });
+    test('response body contains correct text', async () => {
+      expect(createResponse.body.text).toEqual(messagesFields.text);
+    });
+    test('response body contains correct senderId', async () => {
+      expect(createResponse.body.senderId).toEqual(customerFields.id);
+    });
+  });
+
+  describe('GET api/deals/:deal_id/messages', () => {
+    let response;
+    let theMessage;
+    beforeAll(async () => {
+      response = await request
+        .get(`/api/deals/${dealFields.id}/messages`)
+        .auth(authCustomer.accessToken, { type: 'bearer' });
+      theMessage = response.body.filter((m) => m.id === messageFields.id);
+    });
+    test('response with 200 status code', async () => {
+      expect(response.status).toBe(200);
+    });
+    test('response with a json body type', async () => {
+      expect(response.type).toEqual('application/json');
+    });
+    test('response body contains at least one message', async () => {
+      expect(response.body.length).toBeGreaterThan(0);
+    });
+    test('response body contains at least the message created directly to the db', async () => {
+      expect(theMessage[0].text).toEqual(messageFields.text);
     });
   });
 });
